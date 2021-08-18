d5df702adcb1c8e60bebc08e723cd0f47b893176
Vijay Swami <vijay.swami@autodesk.com>
2021-08-18 07:48:21 +0530

    navbar done

---
+++ src/components/tutorial.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Tutorial extends Component {
+  render() {
+    return (
+      <>
+        <h1>This is add Tutorial Component</h1>
+      </>
+    );
+  }
+}
+
+export default Tutorial;
